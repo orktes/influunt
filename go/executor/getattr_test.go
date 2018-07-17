624df3ec731bc47c5a846387740cfddbf35734d9
Jaakko Lukkari <jaakko@unity3d.com>
2018-07-17 15:25:27 +0300

    Remove spew

--- go/executor/getattr_test.go
+++ go/executor/getattr_test.go
@@ -5,15 +5,11 @@ import (
 
 	"github.com/orktes/influunt/go"
 	"github.com/orktes/influunt/go/executor"
-
-	"github.com/davecgh/go-spew/spew"
 )
 
 func TestGetAttr(t *testing.T) {
 	graph := influunt.NewGraph()
 	attNode := influunt.GetAttr(graph, influunt.Const(graph, map[string]interface{}{"foo": 123}), influunt.Const(graph, "foo"))
-
-	spew.Dump(graph)
 
 	executor, err := executor.NewExecutor(graph)
 	if err != nil {
