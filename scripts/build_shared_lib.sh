00b1e0837d6e379e4672259e4cca11d054737f0b
Jaakko Lukkari <jaakko@unity3d.com>
2018-08-01 22:22:55 +0300

    Fix darwin lib build

--- scripts/build_shared_lib.sh
+++ scripts/build_shared_lib.sh
@@ -2,14 +2,22 @@
 
 set -e
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  if hash go 2>/dev/null; then
+    echo "Building darwin amd64"
+    pushd go/python/
+    go build -buildmode=c-shared -o ../../build/sharedlib/darwin/amd64/influunt_core.so
+    popd
+  else
+    echo "Go not installed = no darwin lib build"
+  fi
+else
+  echo "Not running OSX = no darwin lib build"
+fi
+
 docker build -t influunt:latest - < ./scripts/Dockerfile
 docker run --rm -it -v "$GOPATH":/go -w /go/src/github.com/orktes/influunt influunt:latest sh -c '
 cd go/python/
-for GOOS in darwin linux windows; do
-  for GOARCH in 386 amd64; do
-    mkdir -p /go/src/github.com/orktes/influunt/build/sharedlib/$GOOS/$GOARCH
-    echo Building $GOOS for $GOARCH
-    go build -buildmode=c-shared -o /go/src/github.com/orktes/influunt/build/sharedlib/$GOOS/$GOARCH/influunt_core.so
-  done
-done
+echo "Building linux amd64"
+go build -buildmode=c-shared -o /go/src/github.com/orktes/influunt/build/sharedlib/linux/amd64/influunt_core.so
 '
