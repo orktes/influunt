f4b9961a4dc464e891650fea8ea6d9ea1b7cad0b
Jaakko Lukkari <jaakko@unity3d.com>
2018-08-12 14:59:35 +0300

    bdist changes

---
+++ python/README.md
@@ -0,0 +1,2 @@
+# Influunt Python Package
+Dataflow programming for Golang and Python
--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,25 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Distribution
 import os
 import platform
 import sys
 
 VERSION_FILE = os.path.join(os.path.dirname(__file__), 'influunt/VERSION')
-README_MD_FILE = os.path.join(os.path.dirname(__file__), '../README.md')
+README_MD_FILE = os.path.join(os.path.dirname(__file__), 'README.md')
 
+class BinaryDistribution(Distribution):
+    """Auxilliary class necessary to inform setuptools that this is a
+    non-generic, platform-specific package."""
+    def has_ext_modules(self):
+        return True
+
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+    class bdist_wheel(_bdist_wheel):
+        def finalize_options(self):
+            _bdist_wheel.finalize_options(self)
+            self.root_is_pure = False
+except ImportError:
+    bdist_wheel = None
 
 LIB_PATH = []
 
@@ -26,4 +40,6 @@ setup(name='influunt',
       zip_safe=False,
       include_package_data=True,
       data_files=[('influunt/lib', LIB_PATH)],
+      cmdclass={'bdist_wheel': bdist_wheel},
+      distclass=BinaryDistribution,
       )
